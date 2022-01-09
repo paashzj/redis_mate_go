20388ac0201e4504a507502f73dec90d560ceeb7
lovehzj <1922919664@qq.com>
2022-01-09 19:25:48 +0800

    fix kernel log path

--- docker-build/scripts/start-redis.sh
+++ docker-build/scripts/start-redis.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-nohup $REDIS_HOME/redis-server $REDIS_HOME/redis.conf >$REDIS_HOME/redis.log 2>$REDIS_HOME/redis_error.log &
+mkdir $REDIS_HOME/logs
+nohup $REDIS_HOME/redis-server $REDIS_HOME/redis.conf >>$REDIS_HOME/logs/redis.stdout.log 2>>$REDIS_HOME/logs/redis.stderr.log &
+
