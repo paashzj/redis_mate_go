0243ef8aefce8f4d0296bc6eb29bc7a4784209ab
lovehzj <1922919664@qq.com>
2022-01-14 07:17:40 +0800

    Release docker image binary

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export REMOTE_MODE=false
-mkdir $REDIS_HOME/logs
+mkdir -p $REDIS_HOME/logs
 nohup $REDIS_HOME/mate/redis_mate >>$REDIS_HOME/logs/redis_mate.stdout.log 2>>$REDIS_HOME/logs/redis_mate.stderr.log
 
--- docker-build/scripts/start-redis.sh
+++ docker-build/scripts/start-redis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir $REDIS_HOME/logs
+mkdir -p $REDIS_HOME/logs
 nohup $REDIS_HOME/redis-server $REDIS_HOME/redis.conf >>$REDIS_HOME/logs/redis.stdout.log 2>>$REDIS_HOME/logs/redis.stderr.log &
 
